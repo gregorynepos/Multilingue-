94fe42bf37e0010e8c8bdfec2e4799ba4e2e06bc
nepos_a <nepos_a@etna-alternance.net>
2024-06-29 15:34:32 +0200

    update projet

--- .history/app_20240629153250.py
+++ .history/app_20240629153250.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Spécifiez le module WSGI de votre application Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'votre_projet.settings')
 application = get_wsgi_application()
 
